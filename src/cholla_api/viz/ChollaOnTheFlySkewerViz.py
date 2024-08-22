b8c052742751a4483efc91c69dc1594c9ba0c660
digarza <digarza@lux-2.cm.cluster>
2024-08-22 15:16:01 -0700

    correct indentations

--- src/cholla_api/viz/ChollaOnTheFlySkewerViz.py
+++ src/cholla_api/viz/ChollaOnTheFlySkewerViz.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class ChollaOnTheFlyAnalysisViz:
+class ChollaOnTheFlySkewerViz:
     '''
     Cholla OnTheFly Visualization Skewer object
         This object will take Cholla On The Fly Skewer objects and do cool plots
@@ -30,126 +30,127 @@ class ChollaOnTheFlyAnalysisViz:
             (str): image directory suffixed with file name
         '''
 
-        return self.ImgDir + "/" + f"OTFSkew_nOut{nSkewerOutput:.0f}_id{self.OTFSkew.OTFSkewerHead.skew_id:.0f}" + "." + img_ext
+        return self.ImgDir + "/" + f"OTFSkew_nOut{nSkewerOutput:.0f}_{self.OTFSkew.OTFSkewerHead.skew_key[-1]}id{self.OTFSkew.OTFSkewerHead.skew_id:.0f}" + "." + img_ext
 
 
     def plot_combo(self, di, z, save_plot=False, nSkewerOutput=None, img_ext='png'):
         '''
-	Plot all saved quantities in a skewer calculated On-TheFly
+        Plot all saved quantities in a skewer calculated On-TheFly
 
         Args:
-	    di (float): cell size in i-dimension
-	    z (float): current redshift of the skewer
-	    nSkewerOutput (int): (optional) number of the skewer output, used for file name
-	    save_plot (bool): (optional) whether to save plot
+            di (float): cell size in i-dimension
+            z (float): current redshift of the skewer
+            nSkewerOutput (int): (optional) number of the skewer output, used for file name
+            save_plot (bool): (optional) whether to save plot
             img_ext (str): (optional) image file extension
         Returns:
             ...
         '''
 
 	# coordinate positions along line-of-sight
-	los_arr = np.arange(self.OTFSkew.OTFSkewerHead.n_i) * di
+        los_arr = np.arange(self.OTFSkew.OTFSkewerHead.n_i) * di
 
-	# grab datasets
-	density = self.OTFSkew.get_density()
-	HIdensity = self.OTFSkew.get_HIdensity()
-	HeIIdensity = self.OTFSkew.get_HeIIdensity()
-	vel = self.OTFSkew.get_losvelocity()
-	temp = self.OTFSkew.get_temperature()
+        # grab datasets
+        density = self.OTFSkew.get_density()
+        HIdensity = self.OTFSkew.get_HIdensity()
+        HeIIdensity = self.OTFSkew.get_HeIIdensity()
+        vel = self.OTFSkew.get_losvelocity()
+        temp = self.OTFSkew.get_temperature()
 
-	# define limits for each dataset
-	density_scale = -1., 4.
-	HIdensity_scale = -8., 0.
-	HeIIdensity_scale = -6., 3.
-	vel_scale = -300., 300.
-	temp_scale = 3.5, 8.
+        # define limits for each dataset
+        density_scale = -1., 4.
+        HIdensity_scale = -8., 0.
+        HeIIdensity_scale = -6., 3.
+        vel_scale = -350., 350.
+        temp_scale = 3.5, 8.
 
-	fig, ax_all = plt.subplots(nrows=5, ncols=1, figsize=(10,8))
+        fig, ax_all = plt.subplots(nrows=5, ncols=1, figsize=(10,8))
 
-	# define axis objects for each dataset
-	ax_density = ax_all[0]
-	ax_HIdensity = ax_all[1]
-	ax_HeIIdensity = ax_all[2]
-	ax_vel = ax_all[3]
-	ax_temp = ax_all[4]
+        # define axis objects for each dataset
+        ax_density = ax_all[0]
+        ax_HIdensity = ax_all[1]
+        ax_HeIIdensity = ax_all[2]
+        ax_vel = ax_all[3]
+        ax_temp = ax_all[4]
 
-	# scatter plot the data
-	_ = ax_density.scatter(x_arr, np.log10(density_prerestart))
-	_ = ax_HIdensity.scatter(x_arr, np.log10(HIdensity_prerestart))
-	_ = ax_HeIIdensity.scatter(x_arr, np.log10(HeIIdensity_prerestart))
-	_ = ax_vel.scatter(x_arr, vel_prerestart)
-	_ = ax_temp.scatter(x_arr, np.log10(temp_prerestart))
+        # scatter plot the data
+        _ = ax_density.scatter(los_arr, np.log10(density))
+        _ = ax_HIdensity.scatter(los_arr, np.log10(HIdensity))
+        _ = ax_HeIIdensity.scatter(los_arr, np.log10(HeIIdensity))
+        _ = ax_vel.scatter(los_arr, vel)
+        _ = ax_temp.scatter(los_arr, np.log10(temp))
 
+        # apply y lims
+        _ = ax_density.set_ylim(density_scale[0], density_scale[1])
+        _ = ax_HIdensity.set_ylim(HIdensity_scale[0], HIdensity_scale[1])
+        _ = ax_HeIIdensity.set_ylim(HeIIdensity_scale[0], HeIIdensity_scale[1])
+        _ = ax_vel.set_ylim(vel_scale[0], vel_scale[1])
+        _ = ax_temp.set_ylim(temp_scale[0], temp_scale[1])
 
-	# apply y lims
-	_ = ax_density.set_ylim(density_scale[0], density_scale[1])
-	_ = ax_HIdensity.set_ylim(HIdensity_scale[0], HIdensity_scale[1])
-	_ = ax_HeIIdensity.set_ylim(HeIIdensity_scale[0], HeIIdensity_scale[1])
-	_ = ax_vel.set_ylim(vel_scale[0], vel_scale[1])
-	_ = ax_temp.set_ylim(temp_scale[0], temp_scale[1])
-
-	# remove bottom ticks for all but bottom-most plot
-	for ax in ax_prerestart[:-1]:
-	    _ = ax.tick_params(axis='both', which='both', labelbottom=False)
+        # remove bottom ticks for all but bottom-most plot
+        for ax in ax_all[:-1]:
+            _ = ax.tick_params(axis='both', which='both', labelbottom=False)
     
-	# add background grid
-	for ax in ax_prerestart:
-	    _ = ax.grid(which='both', axis='both', alpha=0.3)
+        # add background grid
+        for ax in ax_all:
+            _ = ax.grid(which='both', axis='both', alpha=0.3)
 
-	# define x-label coordinate as 5% of length of skewer
-	xlabel_coord = self.OTFSkew.OTFSkewerHead.n_i * di * 0.05
+        # define x-label coordinate as 5% of length of skewer
+        xlabel_coord = self.OTFSkew.OTFSkewerHead.n_i * di * 0.05
 
-	# add labels with annotations
-	densityunits_str = r'$\log_{10}[h^2 \rm{M}_{\odot} \rm{kpc}^{-3}]$'
-	density_label = r'$\rho - $' + densityunits_str
-	density_coords = xlabel_coord, 2.5
-	_ = ax_density.annotate(density_label, xy=density_coords)
+        # add labels with annotations
+        densityunits_str = r'$\log_{10}[h^2 \rm{M}_{\odot} \rm{kpc}^{-3}]$'
+        density_label = r'$\rho - $' + densityunits_str
+        density_coords = xlabel_coord, 2.5
+        _ = ax_density.annotate(density_label, xy=density_coords)
 
-	HIdensity_label = r'$\rho_{\rm{HI}} - $' + densityunits_str
-	HIdensity_coords = xlabel_coord, -2.5
-	_ = ax_HIdensity.annotate(HIdensity_label, xy=HIdensity_coords)
+        HIdensity_label = r'$\rho_{\rm{HI}} - $' + densityunits_str
+        HIdensity_coords = xlabel_coord, -2.5
+        _ = ax_HIdensity.annotate(HIdensity_label, xy=HIdensity_coords)
 
+        HeIIdensity_label = r'$\rho_{\rm{HeII}} - $' + densityunits_str
+        HeIIdensity_coords = xlabel_coord, 0.5
+        _ = ax_HeIIdensity.annotate(HeIIdensity_label, xy=HeIIdensity_coords)
 
-	HeIIdensity_label = r'$\rho_{\rm{HeII}} - $' + densityunits_str
-	HeIIdensity_coords = xlabel_coord, 0.5
-	_ = ax_HeIIdensity.annotate(HeIIdensity_label, xy=HeIIdensity_coords)
-
-	vel_label = r'$v_{\rm{LOS}} - \rm{km}\ \rm{s}^{-1}$'
-	vel_coords = xlabel_coord, 100.
-	_ = ax_vel.annotate(vel_label, xy=vel_coords)
+        vel_label = r'$v_{\rm{LOS}} - \rm{km}\ \rm{s}^{-1}$'
+        vel_coords = xlabel_coord, 100.
+        _ = ax_vel.annotate(vel_label, xy=vel_coords)
 	
-	temp_label = r'$\rm{T} - \log_{10} [\rm{K}]$'
-	temp_coords = xlabel_coord, 7.
-	_ = ax_temp.annotate(temp_label, xy=temp_coords)
+        temp_label = r'$\rm{T} - \log_{10} [\rm{K}]$'
+        temp_coords = xlabel_coord, 7.
+        _ = ax_temp.annotate(temp_label, xy=temp_coords)
 	
-	# add x label
-	xlabel_str = r"$h^{-1} \rm{kpc}$"
-	ax_temp.set_xlabel(xlabel_str)
+        # add x label
+        xlabel_str = r"$h^{-1} \rm{kpc}$"
+        ax_temp.set_xlabel(xlabel_str)
 
-	# add skew id
-	skew_label = r'$\rm{skewid}$' + rf'$- {skewid}$'
-	skew_coords_y = 2.5
-	skew_coords_x = self.OTFSkew.OTFSkewerHead.n_i * di
-	if skewid < 1e1: # one digit
-	    skew_coords_x *= 0.88
-	elif skewid < 1e2: # two digit
-	    skew_coords_x *= 0.86
-	elif skewid < 1e3: # three digit
-	    skew_coords_x *= 0.85
-	elif skewid < 1e4: # four digit
-	    skew_coords_x *= 0.83
-	elif skewid < 1e5: # five digit
-	    skew_coords_x *= 0.81
-	skew_coords = skew_coords_x, skew_coords_y
-	_ = ax_density.annotate(skew_label, xy=skew_coords)
+        # add skew id
+        skew_label = rf'${self.OTFSkew.OTFSkewerHead.skew_key[-1]}$' 
+        skew_label += r'$\rm{skewid}$' 
+        skew_label += rf'$- {self.OTFSkew.OTFSkewerHead.skew_id}$'
+        skew_coords_y = 2.5
+        skew_coords_x = self.OTFSkew.OTFSkewerHead.n_i * di
+        if self.OTFSkew.OTFSkewerHead.skew_id < 1e1: # one digit
+            skew_coords_x *= 0.86
+        elif self.OTFSkew.OTFSkewerHead.skew_id < 1e2: # two digit
+            skew_coords_x *= 0.84
+        elif self.OTFSkew.OTFSkewerHead.skew_id < 1e3: # three digit
+            skew_coords_x *= 0.83
+        elif self.OTFSkew.OTFSkewerHead.skew_id < 1e4: # four digit
+            skew_coords_x *= 0.81
+        elif self.OTFSkew.OTFSkewerHead.skew_id < 1e5: # five digit
+            skew_coords_x *= 0.79
+        skew_coords = skew_coords_x, skew_coords_y
+        _ = ax_density.annotate(skew_label, xy=skew_coords)
 
-	# add title
-	title_str = rf"$z = {z:.4f}$"
-	_ = fig.suptitle(title_str, y=0.94)
+        # add title
+        title_str = rf"$z = {z:.4f}$"
+        _ = fig.suptitle(title_str, y=0.94)
         _ = fig.tight_layout()
 
         if save_plot:
-            plt.savefig(self.get_combofPath(img_ext), dpi=self.dpi, bbox_inches = "tight")
+            assert nSkewerOutput
+            plt.savefig(self.get_combofPath(nSkewerOutput, img_ext), dpi=self.dpi, bbox_inches = "tight")
         else:
             plt.show()
                            
