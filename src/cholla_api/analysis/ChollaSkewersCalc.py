4172393592744505c8595ccd24b1c91af5625fb5
digarza <digarza@lux-0.cm.cluster>
2024-09-04 15:53:52 -0700

    optical depth has been tested + seems to work on 512 box

--- src/cholla_api/analysis/ChollaSkewersCalc.py
+++ src/cholla_api/analysis/ChollaSkewersCalc.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.special import erf
 
-from cholla_api.analysis.ChollaCosmoGridCalculator import ChollaCosmoCalculator
+from cholla_api.analysis.ChollaHydroCalculator import ChollaHydroCalculator
+from cholla_api.analysis.ChollaCosmoGridCalculator import ChollaSnapCosmologyHead, ChollaCosmoCalculator
 
 
 class ChollaSkewerCosmoCalculator:
@@ -25,30 +26,29 @@ class ChollaSkewerCosmoCalculator:
         self.n_los = n_los
         self.n_ghost = int(0.1 * n_los) # take 10% from bruno
         self.dx = dx
-        self.dtype = dtype
-
-        self.snapHead = snapHead
 
         # number of line-of-sight cells including ghost cells
         self.n_los_ghost = self.n_los + 2 * self.n_ghost
 
-        self.cosmoCalc_ghost = ChollaCosmoCalculator(self.snapHead, cosmoHead)
+        # create ChollaCosmoCalc object
+        self.snapCosmoHead = ChollaSnapCosmologyHead(snapHead, cosmoHead)
+        calc_dims, calc_dims_ghost = (self.n_los,), (self.n_los_ghost,)
+        self.snapCosmoCalc = ChollaCosmoCalculator(self.snapCosmoHead, calc_dims, dtype=dtype)
+        self.snapCosmoCalc_ghost = ChollaCosmoCalculator(self.snapCosmoHead, calc_dims_ghost, dtype=dtype)
+
+        # create HydroCalc objects
+        self.hydroCalc = ChollaHydroCalculator(calc_dims, dtype=dtype)
+        self.hydroCalc_ghost = ChollaHydroCalculator(calc_dims_ghost, dtype=dtype)
 
         # calculate Hubble flow through one cell
-        dvHubble = self.cosmoCalc_ghost.dvHubble(self.dx) # [km s-1]
-        self.dvHubble_cgs = dvHubble * self.cosmoCalc_ghost.cosmoHead.km_cgs # [cm s-1]
+        dvHubble = self.snapCosmoHead.dvHubble(self.dx) # [km s-1]
+        self.dvHubble_cgs = dvHubble * self.snapCosmoHead.cosmoHead.km_cgs # [cm s-1]
 
         # create Hubble flow arrays along left, right, and center of each cell
         # prepend and append ghost cells
-        #self.vHubbleL_ghost = np.arange(-self.n_ghost, self.n_ghost + self.n_los) * self.dvHubble
-        #self.vHubbleR_ghost = vHubbleL + self.dvHubble
-
-        # create Hubble flow array at center of each cell
-        self.vHubbleCenter_ghost_cgs = (np.arange(-self.n_ghost, self.n_ghost + self.n_los) + 0.5) * self.dvHubble_cgs # [cm s-1]
-
-        # calculate & attach current Hubble rate in [km s-1 Mpc-1] and [s-1]
-        self.Hubble = self.cosmoCalc_ghost.Hubble()
-        self.Hubble_cgs = self.Hubble * self.cosmoCalc_ghost.cosmoHead.km_cgs / self.cosmoCalc_ghost.cosmoHead.Mpc_cgs # in cgs [s-1]
+        self.vHubbleL_ghost_cgs = np.arange(-self.n_ghost, self.n_ghost + self.n_los) * self.dvHubble_cgs
+        self.vHubbleR_ghost_cgs = self.vHubbleL_ghost_cgs + self.dvHubble_cgs
+        self.vHubbleC_ghost_cgs = self.vHubbleL_ghost_cgs + 0.5 * self.dvHubble_cgs
 
 
     def extend_ghostcells(self, arr):
@@ -61,8 +61,8 @@ class ChollaSkewerCosmoCalculator:
             arr_ghost (arr): extended array
         '''
 
-        # create array with correct size
-        arr_ghost = self.cosmoCalc_ghost.create_arr()
+        # create array with correct size, inherit arr datatype
+        arr_ghost = np.zeros(self.n_los_ghost, dtype=arr.dtype)
     
         # copy over the real data
         arr_ghost[self.n_ghost : self.n_ghost + self.n_los] = arr[:]
@@ -76,7 +76,7 @@ class ChollaSkewerCosmoCalculator:
         return arr_ghost
 
 
-    def optical_depth_Hydrogen(self, densityHI, velocity_pec, temp):
+    def optical_depth_Hydrogen(self, densityHI, velocity_pec, temp, use_forloop=True):
         '''
         Compute the optical depth for each cell along the line-of-sight
 
@@ -91,61 +91,61 @@ class ChollaSkewerCosmoCalculator:
         assert velocity_pec.size == self.n_los
         assert temp.size == self.n_los
 
-        # introduce hydro calculator
-        hydroCalc = ChollaHydroCalculator(self.n_los, dtype=self.cosmoCalc_ghost.dtype)
-        
-        # convert comoving density to phyiscal density
-        densityHI_phys = self.cosmoCalc_ghost.physical_density(densityHI) # [h2 Msun kpc-3]
-        # convert density to cgs
-        h_cosmo2 = self.cosmoCalc_ghost.cosmoHead.h_cosmo * self.cosmoCalc_ghost.cosmoHead.h_cosmo
-        densityHI_phys_cgs = densityHI_phys * self.cosmoCalc_ghost.cosmoHead.Msun_cgs * h_cosmo2 / self.cosmoCalc_ghost.cosmoHead.kpc3_cgs # [g cm-3]
-        # calculate column number density
-        nHI_phys_cgs = densityHI_phys_cgs / hydroCalc.mp # [cm-3]
-        # extend number density
+        # convert comoving density to physical density then to cgs
+        densityHI_phys = self.snapCosmoCalc.physical_density(densityHI)
+        densityHI_phys_cgs = self.snapCosmoCalc.density_cosmo2cgs(densityHI_phys) # [g cm-3]
+
+        # calculate column number density & extend to ghost cells
+        nHI_phys_cgs = densityHI_phys_cgs / self.hydroCalc.mp # [cm-3]
         nHI_phys_ghost_cgs = self.extend_ghostcells(nHI_phys_cgs)
 
+        # convert peculiar velocity to cgs values & extend to ghost cells
+        velocity_pec_cgs = self.snapCosmoCalc.velocity_cosmo2cgs(velocity_pec)
+        velocity_pec_ghost_cgs = self.extend_ghostcells(velocity_pec_cgs)
+        # convert peculiar to physical velocity by adding Hubble flow
+        velocity_phys_ghost_cgs = velocity_pec_ghost_cgs + self.vHubbleC_ghost_cgs # [cm s-1]
 
-        # extend peculiar velocity
-        velocity_pec_ghost = self.extend_ghostcells(velocity_pec)
-        # convert velocity to cgs
-        velocity_pec_ghost_cgs = velocity_pec_ghost * self.cosmoCalc_ghost.cosmoHead.km_cgs # [cm s-1]
-        # convert to physical velocity by adding Hubble flow
-        velocity_phys_ghost_cgs = velocity_pec_ghost + self.vHubbleCenter_ghost_cgs # [cm s-1]
-        
-
-        # calculate doppler broadening param
-        hydroCalc = ChollaHydroCalculator(self.n_los, dtype=self.cosmoCalc_ghost.dtype)
-        doppler_param_cgs = hydroCalc.Doppler_param_Hydrogen(temp) # [cm s-1]
-        # extend doppler param
+        # calculate doppler broadening param & extend to ghost cells
+        doppler_param_cgs = self.hydroCalc.Doppler_param_Hydrogen(temp) # [cm s-1]
         doppler_param_ghost_cgs = self.extend_ghostcells(doppler_param_cgs)
 
-
         # calculate Ly-alpha interaction cross section
-        sigma_Lya = np.pi * hydroCalc.e * hydroCalc.e / hydroCalc.me # [cm3 g1 s-2 / g] = [cm3 s-2]
-        sigma_Lya = sigma_Lya * hydroCalc.lambda_Lya / hydroCalc.c # [cm3 s-2 * cm / (cm s-1)] = [cm3 s-1]
-        sigma_Lya = sigma_Lya / self.Hubble_cgs # [cm3 s-1 / (s-1)] = [cm3]
+        sigma_Lya = np.pi * self.hydroCalc.e * self.hydroCalc.e / self.hydroCalc.me # [cm3 g1 s-2 / g] = [cm3 s-2]
+        sigma_Lya = sigma_Lya * self.hydroCalc.lambda_Lya / self.hydroCalc.c # [cm3 s-2 * cm / (cm s-1)] = [cm3 s-1]
+        sigma_Lya = sigma_Lya / self.snapCosmoHead.Hubble_cgs # [cm3 s-1 / (s-1)] = [cm3]
         f_12 = 0.416 # oscillator strength
         sigma_Lya *= f_12
 
         # initialize optical depths
-        tau_ghost = np.zeros(self.n_los_ghost, dtype=self.dtype)
+        tau_ghost = self.snapCosmoCalc_ghost.create_arr()
         
-        # fill optical depths with Ly-alpha x-section
-        tau_ghost += sigma_Lya # [cm3]
 
-        for losid in range(self.self.n_los_ghost):
-            # Hubble flow velocity at center of cell
-            vH_losid = self.vHubbleCenter_ghost_cgs[los_id]
-            # find Hubble flow velocity at left and right interfaces of cell
-            vH_L, vH_R = vH_losid - 0.5 * self.dvHubble_cgs, vH_losid + 0.5 * self.dvHubble_cgs
-            # calculate line center shift in terms of broadening scale
-            y_L = (vH_L - velocity_phys_ghost_cgs) / doppler_param_ghost_cgs
-            y_R = (vH_R - velocity_phys_ghost_cgs) / doppler_param_ghost_cgs
-            # [cm3 * # density] = [cm3 * cm-3] = []
-            tau_ghost[losid] *= np.sum(nHI_phys_ghost_cgs * erf(y_R) - erf(y_R)) / 2.0
+        if use_forloop:
+            # OLD IMPLEMENTATION
+            for losid in range(self.n_los_ghost):
+                vH_L, vH_R = self.vHubbleL_ghost_cgs[losid], self.vHubbleR_ghost_cgs[losid]
+                # calculate line center shift in terms of broadening scale
+                y_L = (vH_L - velocity_phys_ghost_cgs) / doppler_param_ghost_cgs
+                y_R = (vH_R - velocity_phys_ghost_cgs) / doppler_param_ghost_cgs
+                # [cm3 * # density] = [cm3 * cm-3] = []
+                tau_ghost[losid] = sigma_Lya * np.sum(nHI_phys_ghost_cgs * (erf(y_R) - erf(y_L))) / 2.0
+        else:        
+            # NEW IMPLEMENTATION w/o for-loop
+            vHL_repeat = np.repeat(self.vHubbleL_ghost_cgs, self.n_los_ghost).reshape((self.n_los_ghost, self.n_los_ghost))
+            vHR_repeat = np.repeat(self.vHubbleR_ghost_cgs, self.n_los_ghost).reshape((self.n_los_ghost, self.n_los_ghost))
+        
+            density_repeat = np.repeat(nHI_phys_ghost_cgs, self.n_los_ghost).reshape((self.n_los_ghost, self.n_los_ghost)).T
+            vel_repeat = np.repeat(velocity_phys_ghost_cgs, self.n_los_ghost).reshape((self.n_los_ghost, self.n_los_ghost)).T
+            doppler_repeat = np.repeat(doppler_param_ghost_cgs, self.n_los_ghost).reshape((self.n_los_ghost, self.n_los_ghost)).T
+        
+            yL_all = (vHL_repeat - vel_repeat) / doppler_repeat
+            yR_all = (vHR_repeat - vel_repeat) / doppler_repeat
+
+            tau_ghost[:] = sigma_Lya * np.sum(density_repeat * (erf(yR_all) - erf(yL_all)), axis=1) / 2.0
+
 
         # clip edges
-        tau = tau_ghost[n_ghost:-n_ghost]
+        tau = tau_ghost[self.n_ghost : -self.n_ghost]
 
         return tau
 
